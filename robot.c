e3b39f34a93456df005d7e501a3d5bdf61d743ef
Nino Schoch <nino.schoch@students.fhv.at>
2015-12-09 15:14:10 +0100

    removed slowly increase function - input for controlling motor cape is calculated in ratio with max controller input

--- robot.c
+++ robot.c
@@ -8,48 +8,21 @@
 #include "includes/DMCC/DMCC.h"
 #include "robot.h"
 
-static const int VELOCITY_STEP = 250; 	///> Size to increase velocity step for step
+#define CONTROLLER_MAX		500			///> Highest input of controller to calculate in ratio
+
 static int session = -1; 				///> Session received by DMCCstart
-
-/**
- * Slowly increase velocity of motor
- * too lower increase of current intensity
- */
-static void slowly_increase_velocity(int motor, int pwm) {
-    int currentVel = getTargetVel(session, motor);
-    int targetVel = pwm; //TODO: safe clause of too high or too low?
-
-    int difVel = targetVel - currentVel;
-    int temp = 0;
-    // slowly increase speed based on VELOCITY_STEP
-    while (difVel != 0) {
-        temp = difVel % VELOCITY_STEP;
-        if (temp == 0) {
-            difVel -= VELOCITY_STEP;
-            currentVel += VELOCITY_STEP;
-        } else {
-            difVel -= temp;
-            currentVel += temp;
-        }
-
-        setMotorPower(session, motor, currentVel);
-        //TODO: sleep some ms
-    }
-}
 
 /**
  * Calculate velocity of given input
  * based on highest input of controller
  */
 static void set_velocity(int motor, int input) {
-    //TODO: integrate highest input of controller
-    double highestControllerInput = 500;
-    double velRatio = input / highestControllerInput;
+    double velRatio = input / (double) CONTROLLER_MAX;
     if (velRatio > 1) {
     	velRatio = 1;
     }
 
-    return slowly_increase_velocity(motor, velRatio * VELOCITY_MAX);
+    setMotorPower(session, motor, velRatio * VELOCITY_MAX);
 }
 
 void robot_init() {
@@ -86,7 +59,6 @@ void robot_drive_right(int input) {
 void robot_turn_left() {
 	robot_connect();
 
-    //robot_stop();
     setMotorPower(session, MOTOR_LEFT, VELOCITY_MIN);
     setMotorPower(session, MOTOR_RIGHT, VELOCITY_MAX);
 }
@@ -94,7 +66,6 @@ void robot_turn_left() {
 void robot_turn_right() {
 	robot_connect();
 
-    //robot_stop();
     setMotorPower(session, MOTOR_LEFT, VELOCITY_MAX);
     setMotorPower(session, MOTOR_RIGHT, VELOCITY_MIN);
 }
